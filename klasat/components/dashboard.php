e38d80c2475105fcd9606b714685675b4d13036b
Leon Shala <ls48316@ubt-uni.net>
2021-06-04 23:11:51 +0200

    dashboard

--- klasat/components/dashboard.php
+++ klasat/components/dashboard.php
@@ -2,9 +2,28 @@
     include './header.php'
 ?>
 
-<content>
-<h1>DASHBOARD</h1>
-</content>
+<!DOCTYPE html>
+<head>
+
+</head>
+<body style="background-color:#272727;">
+<div id="users">
+<table>
+
+
+</table>
+
+</div>
+</body>
+
+
+
+
+
+</html>
+
+
+
 
 <?php 
     include './footer.php'
