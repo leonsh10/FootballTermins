f16dfa208fb4d469a751c18d8bc35384379957e5
Leon Shala <ls48316@ubt-uni.net>
2021-06-05 11:44:44 +0200

    votuesitTable

--- klasat/components/dashboard.php
+++ klasat/components/dashboard.php
@@ -1,17 +1,64 @@
 <?php 
-    include './header.php'
+include_once '../../php/userMapper.php';
+include_once '../../php/dboconfig.php';
+include_once '../../php/LoginRegister/simpleUserClass.php';
+  
+
+session_start();
+// $emri=$_SESSION['username'];
+$mapper =  new UserMapper();
+$userList = $mapper->getAllUsers();
+include './header.php'
 ?>
+
 
 <!DOCTYPE html>
 <head>
-
+<title>Dashboard</title>
+<link rel="stylesheet" href="../../stilizimi/dashboard.css">
 </head>
 <body style="background-color:#272727;">
 <div id="users">
-<table>
-
-
-</table>
+<div style="display:flex;justify-content:center;">
+<h2 id="titulliTabeles">Votuesit</h2>
+</div>
+        <table>
+            <thead>
+                <tr>
+                    <td >ID</td>
+                    <td >Emri</td>
+                    <td >Mbiemri</td>
+                    <td>Username</td>
+                    <td>Email</td>
+                    <td>Role</td>
+                    <td >Fshij/Edit</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                            
+                            if (is_array($userList) || is_object($userList))
+                        {
+                            foreach ($userList as $user) {
+                                ?>
+                                    <tr>
+                                    <td><?php echo $user['id_user']; ?></td>
+                                        <td><?php echo $user['emri']; ?></td>
+                                        <td><?php echo $user['mbiemri']; ?></td>
+                                        <td><?php echo $user['username']; ?></td>
+                                        <td><?php echo $user['email']; ?></td>
+                                        <td><?php echo $user['role']; ?></td>
+                                        <td><button id="butonFshij" style="margin-bottom:10px;"><a style="text-decoration:none;" href=<?php echo "../../php/LoginRegister/deleteUser.php?id=" . $user['id_user'];
+                                                    ?>>Fshij</button>
+                                                    <button id="butonFshij"><a style="text-decoration:none;" href="#">Edit</button></td>
+                                                   
+                                    </tr>
+                                <?php
+                                }
+                            }
+                                ?>
+            </tbody>
+        </table>
 
 </div>
 </body>
@@ -28,3 +75,4 @@
 <?php 
     include './footer.php'
 ?>
+
